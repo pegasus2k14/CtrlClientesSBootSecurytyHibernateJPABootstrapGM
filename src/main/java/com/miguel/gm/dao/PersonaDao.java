ec64395df80e7bc6906b4cbddec2a83122f42c1a
Miguel R <samuraiequis@yahoo.com>
2020-09-08 16:51:44 -0600

    Agregada Menu Bar Bootstrap

--- src/main/java/com/miguel/gm/dao/PersonaDao.java
+++ src/main/java/com/miguel/gm/dao/PersonaDao.java
@@ -2,7 +2,7 @@ package com.miguel.gm.dao;
 
 import com.miguel.gm.domain.Persona;
 import org.springframework.data.repository.CrudRepository;
-
+//
 public interface PersonaDao extends CrudRepository<Persona, Long>{
     
 }
